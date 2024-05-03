03bb963a7fa81d90d77d0ccaa066f6db0c1fdc32
= <=>
2024-05-03 13:47:43 -0300

    [feat] - added foods component

---
+++ apps/nutrition-frontend/src/app/modules/foods/foods.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nutrition-app-foods',
+  standalone: true,
+  templateUrl: './foods.component.html',
+  styleUrls: ['./foods.component.scss']
+})
+export class FoodsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
