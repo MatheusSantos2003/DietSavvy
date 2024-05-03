72bf9f8dcc8794ba5b6d255f2245f9ad976397c1
= <=>
2024-05-03 13:48:01 -0300

    [feat] - added diet component

---
+++ apps/nutrition-frontend/src/app/modules/diet/diet.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nutrition-app-diet',
+  standalone: true,
+  templateUrl: './diet.component.html',
+  styleUrls: ['./diet.component.scss']
+})
+export class DietComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
